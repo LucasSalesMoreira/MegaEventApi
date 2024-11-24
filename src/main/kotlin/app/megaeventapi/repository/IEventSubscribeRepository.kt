141ec78fe955d2e692b6c29116599fc8ae9fa074
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2024-11-24 17:05:40 -0300

    feat: Adiciona crinscrição em eventos
    Signed-off-by: LucasSalesMoreira <lucasdevsoftware@gmail.com>

---
+++ src/main/kotlin/app/megaeventapi/repository/IEventSubscribeRepository.kt
@@ -0,0 +1,13 @@
+package app.megaeventapi.repository
+
+import app.megaeventapi.model.entity.Event
+import app.megaeventapi.model.entity.EventSubscribe
+import app.megaeventapi.model.entity.User
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.util.Optional
+
+@Repository
+interface IEventSubscribeRepository: CrudRepository<EventSubscribe, String> {
+    fun findByEventAndUser(event: Event, user: User): Optional<EventSubscribe>
+}
